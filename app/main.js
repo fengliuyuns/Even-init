d846cd4cd8d4f32c6ca8c47bd13b092805a4da4e
He Fang <hefang@lonwin.org>
2017-12-05 18:21:59 +0800

    init code

--- app/main.js
+++ app/main.js
@@ -4,4 +4,7 @@ import {render} from 'react-dom';
 import Greeter from './Greeter';
 import './main.css';//使用require导入css文件
 
-render(<Greeter />, document.getElementById('root'));
+render(
+	<Greeter />, 
+	document.getElementById('root')
+);
